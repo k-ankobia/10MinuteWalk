3b3b287c9d9a2bff4f40ecfa00738e9cb30ac2a9
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-26 14:15:06 +0100

    Passed test for it assigns coordinates for 1 for north and east

--- lib/walk_app.rb
+++ lib/walk_app.rb
@@ -1,6 +1,24 @@
 class Walk 
+  attr_reader :x_coordinate, :y_coordinate
+
+  CORD = {
+    'n' => 1,
+    's' => -1,
+    'e' => 1,
+    'w' => -1
+  }
+  
+
+  def initialize(x_coordinate = 0, y_coordinate = 0)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+  end
 
   def ten_minute_walk(direction)
     false if direction.length != 10
+  end
+
+  def coordiantes(val)
+  CORD[val]
   end
 end
--- spec/walk_app_spec.rb
+++ spec/walk_app_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe Walk do
       direction = ['w', 's', 'e', 'e', 'n', 'n']
       expect(app.ten_minute_walk(direction)).to eq false
     end
+
+    it 'initialise the app with values of zero for x and y cordinates' do 
+      expect(app.x_coordinate).to eq 0
+      expect(app.y_coordinate).to eq 0
+    end
+
+    it 'assigns coordinates of 1 for n e' do 
+      expect(app.coordiantes('n')).to eq 1
+      expect(app.coordiantes('e')).to eq 1
+    end
   end
 end
